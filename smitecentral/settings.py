2825215d3bca71e682c3aeef08ac47caf48b1a8c
Fidel Elie <38261325+FidelElie@users.noreply.github.com>
2020-08-04 19:57:32 +0100

    Updated Whitenoise configuration

--- smitecentral/settings.py
+++ smitecentral/settings.py
@@ -126,13 +126,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(PROJECT_DIR, "static/")]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 

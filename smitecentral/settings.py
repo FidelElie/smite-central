a54a7a16b599bb7a0c2765c21a5be318a78e61e0
Fidel Elie <38261325+FidelElie@users.noreply.github.com>
2020-08-04 19:38:57 +0100

    Changed Static Root to Whitenoise Example

--- smitecentral/settings.py
+++ smitecentral/settings.py
@@ -132,6 +132,8 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(PROJECT_DIR, "static")]
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 MEDIA_URL = "/media/"

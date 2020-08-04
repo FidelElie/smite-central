fa18203e3459862453d808e1b0888a997afa7db1
Fidel Elie <38261325+FidelElie@users.noreply.github.com>
2020-08-04 18:49:41 +0100

    Updated Hosts

--- smitecentral/settings.py
+++ smitecentral/settings.py
@@ -35,7 +35,7 @@ DATABASE_URI = os.getenv("DATABASE_URI")
 
 SMITE_VOD_ID = "UCuXuve8eXPmwCkrOF32JpjQ"
 
-ALLOWED_HOSTS = ["https://smite-central.herokuapp.com/"]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', "https://smite-central.herokuapp.com/"]
 
 # Application definition
 

8f8578e18d3c5225e8bea2d845bde3bda280026c
Fidel Elie <38261325+FidelElie@users.noreply.github.com>
2020-08-04 19:36:57 +0100

    Removed Static Root

--- smitecentral/settings.py
+++ smitecentral/settings.py
@@ -132,8 +132,6 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(PROJECT_DIR, "static")]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 MEDIA_URL = "/media/"

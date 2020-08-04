822f9a4d1218ee1f5e3a99500d94821e182f15f8
Fidel Elie <38261325+FidelElie@users.noreply.github.com>
2020-08-04 18:31:19 +0100

    Moved Static Folder And Added Root

--- smitecentral/settings.py
+++ smitecentral/settings.py
@@ -125,9 +125,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "public/static/")]
+STATICFILES_DIRS = [os.path.join(PROJECT_DIR, "static/")]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "public/static/")
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "public/media/")
 

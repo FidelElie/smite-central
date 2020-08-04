00b690b3b1a2d76def560062b7518e7c639e4b99
Fidel Elie <38261325+FidelElie@users.noreply.github.com>
2020-08-04 19:43:29 +0100

    Edited File Paths

--- smitecentral/settings.py
+++ smitecentral/settings.py
@@ -130,11 +130,11 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(PROJECT_DIR, "static")]
+STATICFILES_DIRS = [os.path.join(PROJECT_DIR, "static/")]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 MEDIA_URL = "/media/"
 

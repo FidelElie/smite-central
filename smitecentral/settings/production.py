4bae01458dbd85e3e8b23d367408fa327dad1884
Fidel Elie <38261325+FidelElie@users.noreply.github.com>
2020-08-07 12:09:19 +0100

    Chaned Media Storage

--- smitecentral/settings/production.py
+++ smitecentral/settings/production.py
@@ -1,7 +1,6 @@
 import os
 
 import dj_database_url
-import storages
 
 from .base import *
 
@@ -12,8 +11,9 @@ YOUTUBE_API_KEY = os.getenv('YOUTUBE_API_KEY')
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+AWS_DEFAULT_ACL = None
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
 ALLOWED_HOSTS += ['.herokuapp.com']

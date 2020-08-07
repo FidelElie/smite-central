80ae4871f2aca932da733100539eebc5c368366f
Fidel Elie <38261325+FidelElie@users.noreply.github.com>
2020-08-07 12:28:49 +0100

    Changed default storage

--- smitecentral/settings/production.py
+++ smitecentral/settings/production.py
@@ -27,5 +27,5 @@ DATABASES['default'] = dj_database_url.config(
 )
 
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'smitecentral.storage.MediaStorage'
 
